9895ed05c9bffee204841d425420b4afd0ac15a0
Fariz JR <87758609+farizjr1@users.noreply.github.com>
2021-08-28 19:00:30 +0700

    Update veruscoin.sh

--- veruscoin.sh
+++ veruscoin.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 sudo apt update
 sudo apt install screen -y
-wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
-tar xf hellminer_cpu_linux.tar.gz
-./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RQQviv9iBAd8DVRC1WfqVj31cfDtYW5p1i.farizjr -p x --cpu 2
-while [ 1 ]; do
-sleep 3
-done
-sleep 999
+wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz
+tar -xvf nheqminer-Linux-v0.8.2.tgz
+tar -xvf nheqminer-Linux-v0.8.2.tar.gz
+nheqminer/nheqminer -v -l na.luckpool.net:3956 -u RQQviv9iBAd8DVRC1WfqVj31cfDtYW5p1i.farizjr -p x -t 1
