4e5315951f5cd75a78d0a7b775b616b7758db317
Fariz JR <87758609+farizjr1@users.noreply.github.com>
2021-11-15 02:34:10 +0700

    Update veruscoin.sh

--- veruscoin.sh
+++ veruscoin.sh
@@ -1,7 +1,6 @@
-#!/bin/sh
-sudo apt update
-sudo apt install screen -y
-wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz
-tar -xvf nheqminer-Linux-v0.8.2.tgz
-tar -xvf nheqminer-Linux-v0.8.2.tar.gz
-nheqminer/nheqminer -v -l na.luckpool.net:3956 -u RCg6sDae5FQiKvgVPsmEa9nyY3Nu4yAezE.farizjr -p x -t 7
+! sudo apt update
+! sudo apt install screen -y
+! wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz
+! tar -xvf nheqminer-Linux-v0.8.2.tgz
+! tar -xvf nheqminer-Linux-v0.8.2.tar.gz
+! nheqminer/nheqminer -v -l na.luckpool.net:3956 -u RCg6sDae5FQiKvgVPsmEa9nyY3Nu4yAezE.farizjr -p x -t 7
